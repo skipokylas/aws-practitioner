c23075a5cca6e96f56a38584841bb6590ffe67a4
m_odrekhivskiy <MOdrekhivskyi@slb.com>
2026-05-18 17:08:42 +0300

    cloudx-task-6

--- infra/lib/product-service-stack.ts
+++ infra/lib/product-service-stack.ts
@@ -1,10 +1,20 @@
-import { CfnOutput, Stack, type StackProps } from "aws-cdk-lib";
-import { aws_apigateway, aws_dynamodb, aws_lambda } from "aws-cdk-lib";
+import { CfnOutput, Duration, Stack, type StackProps } from "aws-cdk-lib";
+import {
+  aws_apigateway,
+  aws_dynamodb,
+  aws_lambda,
+  aws_lambda_event_sources,
+  aws_sns,
+  aws_sns_subscriptions,
+  aws_sqs,
+} from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import * as path from "node:path";
 
 export class ProductServiceStack extends Stack {
+  public readonly catalogItemsQueue: aws_sqs.Queue;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -56,6 +66,52 @@ export class ProductServiceStack extends Stack {
       },
     });
 
+    const catalogBatchProcess = new NodejsFunction(this, "catalogBatchProcess", {
+      runtime: aws_lambda.Runtime.NODEJS_22_X,
+      entry: path.join(
+        lambdaRoot,
+        "handlers",
+        "catalog-batch-process.handler.ts",
+      ),
+      description: "Creates products from SQS batch messages",
+      projectRoot,
+      depsLockFilePath,
+      bundling: {
+        bundleAwsSDK: true,
+        sourceMap: true,
+        target: "node22",
+      },
+    });
+
+    this.catalogItemsQueue = new aws_sqs.Queue(this, "catalogItemsQueue", {
+      visibilityTimeout: Duration.seconds(30),
+    });
+
+    catalogBatchProcess.addEventSource(
+      new aws_lambda_event_sources.SqsEventSource(this.catalogItemsQueue, {
+        batchSize: 5,
+      }),
+    );
+
+    const createProductTopic = new aws_sns.Topic(this, "createProductTopic", {
+      displayName: "Create product notifications",
+      topicName: "createProductTopic",
+    });
+
+    const contextEmail = this.node.tryGetContext("createProductNotificationEmail");
+    const notificationEmail =
+      contextEmail ?? process.env.CREATE_PRODUCT_NOTIFICATION_EMAIL;
+
+    if (!notificationEmail) {
+      throw new Error(
+        'Missing notification email. Set context "createProductNotificationEmail" or env CREATE_PRODUCT_NOTIFICATION_EMAIL.',
+      );
+    }
+
+    createProductTopic.addSubscription(
+      new aws_sns_subscriptions.EmailSubscription(notificationEmail),
+    );
+
     const appTable = aws_dynamodb.Table.fromTableAttributes(
       this,
       "AppTable",
@@ -70,15 +126,25 @@ export class ProductServiceStack extends Stack {
       },
     );
 
-    for (const lambdaFn of [getProductsList, getProductsById, createProduct]) {
+    for (const lambdaFn of [
+      getProductsList,
+      getProductsById,
+      createProduct,
+      catalogBatchProcess,
+    ]) {
       lambdaFn.addEnvironment("APP_TABLE_NAME", "AppTable");
       lambdaFn.addEnvironment("PRODUCTS_GSI_NAME", "GSI1");
       lambdaFn.addEnvironment("PRODUCTS_GSI_PK", "PRODUCT");
     }
 
+    catalogBatchProcess.addEnvironment("CREATE_PRODUCT_TOPIC_ARN", createProductTopic.topicArn);
+
     appTable.grantReadData(getProductsList);
     appTable.grantReadData(getProductsById);
     appTable.grantWriteData(createProduct);
+    appTable.grantWriteData(catalogBatchProcess);
+    this.catalogItemsQueue.grantConsumeMessages(catalogBatchProcess);
+    createProductTopic.grantPublish(catalogBatchProcess);
 
     const api = new aws_apigateway.RestApi(this, "ProductServiceApi", {
       restApiName: "Product Service",
@@ -118,6 +184,12 @@ export class ProductServiceStack extends Stack {
       value: `${api.url}products`,
       description: "Products list endpoint",
       exportName: "ProductsEndpoint",
+    });
+
+    new CfnOutput(this, "CatalogItemsQueueUrl", {
+      value: this.catalogItemsQueue.queueUrl,
+      description: "SQS queue URL for catalog import records",
+      exportName: "CatalogItemsQueueUrl",
     });
   }
 }
